15d9cf2a5154c43003d21bea4eb6a65376a91b00
abhizala <abhizala@hotmail.com>
2020-11-07 23:01:08 +0530

    b4s4: ExtendedUI Bringup

---
+++ exui_sargo.mk
@@ -0,0 +1,45 @@
+ # Copyright (C) 2020 The ExtendedUI Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit some common ExtendedUI stuff.
+$(call inherit-product, vendor/exui/config/common.mk)
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, device/google/bonito/aosp_sargo.mk)
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
+# Device specific stuff
+TARGET_BOOT_ANIMATION_RES := 1080
+USE_GAPPS := true
+EXUI_BUILDTYPE := OFFICIAL
+
+PRODUCT_NAME := exui_sargo
+PRODUCT_DEVICE := sargo
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Pixel 3a
+PRODUCT_MANUFACTURER := Google
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME="sargo" \
+    PRIVATE_BUILD_DESC="sargo-user 11 RP1A.201105.002 6869500 release-keys"
+
+BUILD_FINGERPRINT := "google/sargo/sargo:11/RP1A.201105.002/6869500:user/release-keys"
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.fingerprint=google/sargo/sargo:11/RP1A.201105.002/6869500:user/release-keys
+
+$(call inherit-product-if-exists, vendor/google/sargo/sargo-vendor.mk)
